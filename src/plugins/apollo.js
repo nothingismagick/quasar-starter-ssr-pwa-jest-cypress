d74e8795d2e1c4eb0698e1e5283d201d8e78c6bc
D.C. Thompson <denjell@kinokabaret.com>
2018-08-24 00:29:21 +0200

    Updated readme - use node-fetch to enable apollo
    Signed-off-by: D.C. Thompson <denjell@kinokabaret.com>

--- src/plugins/apollo.js
+++ src/plugins/apollo.js
@@ -1,11 +1,15 @@
 import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
+// import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import VueApollo from 'vue-apollo'
 
-const httpLink = new HttpLink({
-  uri: 'http://localhost:4000'
-})
+import fetch from 'node-fetch';
+import { createHttpLink } from 'apollo-link-http';
+const httpLink = createHttpLink({ uri: 'http://localhost:4000', fetch: fetch });
+
+// const httpLink = new HttpLink({
+//  uri: 'http://localhost:4000'
+// })
 
 // Create the apollo client
 const apolloClient = new ApolloClient({
