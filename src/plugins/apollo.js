be6ed81d1646c79ed9e120697b940e327399d96f
Kevin <contact@kevinmarrec.com>
2018-08-24 22:28:21 +0200

    Remove Extra semicolons [eslint]

--- src/plugins/apollo.js
+++ src/plugins/apollo.js
@@ -1,10 +1,10 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import VueApollo from 'vue-apollo'
-import fetch from 'node-fetch';
-import { createHttpLink } from 'apollo-link-http';
+import fetch from 'node-fetch'
+import { createHttpLink } from 'apollo-link-http'
 
-const httpLink = createHttpLink({ uri: 'http://localhost:4000', fetch: fetch });
+const httpLink = createHttpLink({ uri: 'http://localhost:4000', fetch: fetch })
 
 // Create the apollo client
 const apolloClient = new ApolloClient({
